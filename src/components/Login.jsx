98265082cbe9fb114520463a5e5ae26aaea351e0
Ulugbek <mirzarakhmanoff@gmail.com>
2024-09-04 10:14:37 +0500

    bugs fixed

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,18 +2,21 @@ import React from "react";
 import { Button, Checkbox, Form, Input } from "antd";
 import axios from "../api";
 import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   let token = useSelector((state) => state.token);
   console.log(token);
+
   const onFinish = (values) => {
     axios
       .post("/auth/login", values)
-
       .then((res) => {
         console.log(res.data.token);
         dispatch({ type: "LOGIN", payload: res.data.token });
+        navigate("/");
       })
       .catch((err) => console.log(err));
   };
@@ -23,21 +26,14 @@ const Login = () => {
   };
 
   return (
-    <div className="mx-auto flex justify-center items-center h-[100vh]">
-      <div className="w-[500px]">
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md p-8 bg-white shadow-lg rounded-lg">
+        <h2 className="text-2xl font-semibold text-center text-gray-800 mb-6">
+          Login to Your Account
+        </h2>
         <Form
-          className="mx-auto"
           name="basic"
           layout="vertical"
-          labelCol={{
-            span: 8,
-          }}
-          wrapperCol={{
-            span: 16,
-          }}
-          style={{
-            maxWidth: 600,
-          }}
           initialValues={{
             remember: true,
           }}
@@ -55,7 +51,7 @@ const Login = () => {
               },
             ]}
           >
-            <Input />
+            <Input className="p-2 border rounded-md" />
           </Form.Item>
 
           <Form.Item
@@ -68,27 +64,19 @@ const Login = () => {
               },
             ]}
           >
-            <Input.Password />
+            <Input.Password className="p-2 border rounded-md" />
           </Form.Item>
 
-          <Form.Item
-            name="remember"
-            valuePropName="checked"
-            wrapperCol={{
-              offset: 8,
-              span: 16,
-            }}
-          >
+          <Form.Item name="remember" valuePropName="checked">
             <Checkbox>Remember me</Checkbox>
           </Form.Item>
 
-          <Form.Item
-            wrapperCol={{
-              offset: 8,
-              span: 16,
-            }}
-          >
-            <Button type="primary" htmlType="submit">
+          <Form.Item>
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="w-full bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 rounded-lg"
+            >
               Submit
             </Button>
           </Form.Item>
